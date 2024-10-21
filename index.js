bb95794c03036779a81662042af8bf8cacd73121
Michel Milezzi <4431242+michelmilezzi@users.noreply.github.com>
2024-10-21 19:17:55 -0300

    feat: add integration test and some refac

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const errorHandling = require("./middleware/errorHandling");
+const WithdrawalService = require("./services/withdrawalService");
 
 const app = express();
 const port = 3000;
-const errorHandling = require("./middleware/errorHandling");
-const WithdrawalService = require("./services/withdrawalService");
 const withdrawalService = new WithdrawalService();
 
 app.use(bodyParser.json());
@@ -13,13 +13,14 @@ app.post("/withdrawals", async (req, res, next) => {
   try {
     const { serializable = false } = req.query;
     const { amount, user } = req.body;
+    const executeUsingSerializableIsolationLevel = serializable === "true";
 
     const newWithdrawal = await withdrawalService.handleWithdrawalRequest(
       {
         amount,
         user,
       },
-      serializable
+      executeUsingSerializableIsolationLevel
     );
 
     return res.status(201).json(newWithdrawal);
@@ -30,6 +31,8 @@ app.post("/withdrawals", async (req, res, next) => {
 
 app.use(errorHandling);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = { server, app };
